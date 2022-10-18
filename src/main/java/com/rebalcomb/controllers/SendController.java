33d9eaa1258a60b79c5c87cd1f963e66fd8e69ea
hmanspie <hmanspie@gmail.com>
2022-10-18 15:04:11 +0300

    Chat start

--- src/main/java/com/rebalcomb/controllers/SendController.java
+++ src/main/java/com/rebalcomb/controllers/SendController.java
@@ -1,2 +1,41 @@
-package com.rebalcomb.controllers;public class SendController {
+package com.rebalcomb.controllers;
+
+import com.rebalcomb.model.dto.NewMessageRequest;
+import com.rebalcomb.service.MessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class SendController {
+    private Logger logger = LoggerFactory.getLogger(SendController.class);
+    private final MessageService messageService;
+
+    @Autowired
+    public SendController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/sendNewMessage")
+    public ModelAndView login(ModelAndView model, @Valid @ModelAttribute NewMessageRequest newMessageRequest) throws InterruptedException {
+        if(messageService.sendMessage(newMessageRequest)){
+            model.addObject("messages", messageService.findAllBySender(AccountController.activeAccount));
+            model.addObject("headPageValue", "outcoming");
+            model.setViewName("headPage");
+            logger.info("Message sent successfully!");
+            return model;
+        }else{
+            model.addObject("headPageValue", "write");
+            model.addObject("newMessageRequest", new NewMessageRequest());
+            model.setViewName("headPage");
+            logger.error("Message not sent!");
+            return model;
+        }
+    }
 }
