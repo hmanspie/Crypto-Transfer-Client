0680265280914181bf315321ff642166fcf6fdaa
Vladyslav <vladkalych@gmail.com>
2022-10-27 09:30:55 +0300

    Removed unused imports Updated address service

--- src/main/java/com/rebalcomb/email/EmailUtil.java
+++ src/main/java/com/rebalcomb/email/EmailUtil.java
@@ -1,21 +1,11 @@
 package com.rebalcomb.email;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.activation.FileDataSource;
-import javax.mail.BodyPart;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import java.util.Date;
 
 /**
  * @author Alexandr Bratchyk
